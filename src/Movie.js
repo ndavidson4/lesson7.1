d396c0210e9f956f1bacfe71b2af2f4de80ab160
natalie <natalie.davidson.55@gmail.com>
2024-04-10 13:49:47 -0400

    firstCommit

---
+++ src/Movie.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Movie.css';
+
+function Movie(){
+  return (
+    <div className="Movie">
+      <h2>Movie Title: The Batman Movie</h2>
+      <p>Director: Matt Reeves</p>
+      <p>Stars: Robert Pattinson, Zoe Kravitz</p>
+    </div>
+  );
+}
+
+export default Movie;
