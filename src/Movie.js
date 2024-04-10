0ddc59d73e907f139568dec75f6e14d35240e728
natalie <natalie.davidson.55@gmail.com>
2024-04-10 17:15:17 -0400

    commit4

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,15 @@
 function Movie() {
   return (
       <div className="Movie">
+
           <h2>Movie Title: The Batman</h2>
+
           <h4>Director: Matt Reeves</h4>
+          
           <p>Stars: Robert Pattinson, Zoe Kravitz</p>
+
       </div>
   );
 }
+
 export default Movie;
