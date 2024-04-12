2a8ae06d59bcc9d49e360e245c4674d1ba20d9be
ndavidson4 <nsd5240@psu.edu>
2024-04-11 21:46:30 -0400

    6committh

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Movie.css';
 import Movie from './Movie.js';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 //<Route path='https://ndavidson4.github.io/lesson7.1/' element={<RequireAuthRoute><Explorer /></RequireAuthRoute>}/>
 
 function App() {
   return (
     <div className="App">
-      <h1>List of Movies:</h1>
+      <Router>
+        <h1>List of Movies:</h1>
 
-      <div className="Movies row">
+        <div className="Movies row">
 
-        <div className="col-lg-4">
-          <Movie  /> 
-        </div>
+          <div className="col-lg-4">
+            <Movie  /> 
+          </div>
 
-        <div className="col-lg-4">
-          <Movie  /> 
-        </div>
+          <div className="col-lg-4">
+            <Movie  /> 
+          </div>
 
-        <div className="col-lg-4">
-          <Movie  /> 
-        </div> 
+          <div className="col-lg-4">
+            <Movie  /> 
+          </div> 
       </div>
+      </Router>
     </div>
   );
 }
